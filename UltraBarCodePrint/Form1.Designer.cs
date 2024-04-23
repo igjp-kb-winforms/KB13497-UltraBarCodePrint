7d49137af1c8aa06f0a0bb6a03159f672f6b59ab
mkamiishi <mkamiishi@infragistics.com>
2024-04-23 16:47:07 +0900

    初回コミット
    Initial commit

---
+++ UltraBarCodePrint/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace UltraBarCodePrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.ultraQRCodeBarcode1 = new Infragistics.Win.DataVisualization.UltraQRCodeBarcode();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(187, 359);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(75, 21);
+            this.buttonPrint.TabIndex = 1;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // ultraQRCodeBarcode1
+            // 
+            this.ultraQRCodeBarcode1.Data = "http://infragistics.com";
+            this.ultraQRCodeBarcode1.ErrorMessageText = "無効な値です。ヘルプを参照して、有効なバーコードの Data プロパティ値構造を確認してください。";
+            this.ultraQRCodeBarcode1.Location = new System.Drawing.Point(72, 12);
+            this.ultraQRCodeBarcode1.Name = "ultraQRCodeBarcode1";
+            this.ultraQRCodeBarcode1.Size = new System.Drawing.Size(300, 300);
+            this.ultraQRCodeBarcode1.TabIndex = 2;
+            this.ultraQRCodeBarcode1.XDimension = 0.265D;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 406);
+            this.Controls.Add(this.ultraQRCodeBarcode1);
+            this.Controls.Add(this.buttonPrint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonPrint;
+        private Infragistics.Win.DataVisualization.UltraQRCodeBarcode ultraQRCodeBarcode1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
---
+++ UltraBarCodePrint/Form1.cs
@@ -0,0 +1,42 @@
+﻿namespace UltraBarCodePrint
+{
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Drawing.Printing;
+    using System.IO;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            this.InitializeComponent();
+
+            ultraQRCodeBarcode1.Data = "This is a test QR code.";
+        }
+
+        private void buttonPrint_Click(object sender, System.EventArgs e)
+        {
+            printDocument1.Print();
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Image image;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // バーコードをMemoryStreamに保存する
+                ultraQRCodeBarcode1.SaveTo(ms, ImageFormat.Bmp);
+                // MemoryStreamからImageを生成する
+                image = Image.FromStream(ms);
+            }
+
+            //画像を描画する
+            e.Graphics.DrawImage(image, new Point(0, 0));
+            e.HasMorePages = false;
+            image.Dispose();
+        }
+    }
+}
